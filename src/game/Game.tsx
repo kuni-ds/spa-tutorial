066500d2115a45153fce80b5271e7e6a16ee36d6
Kunihito Terada <k.terada@digitalshift.co.jp>
2023-05-26 09:34:09 +0900

    コメント削除

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -23,49 +23,6 @@ export default function Game(props) {
 
   let status = turn ? "赤" : "青";
 
-  // const judgeVictory = (): string => {
-  //   const color = turn ? "青" : "赤";
-  //   //よこ
-  //   for (let i = 0; i < 3; i++) {
-  //     let victory = true;
-  //     for (let j = 0; j < 3; j++) {
-  //       if (squares[i][j] !== color) {
-  //         victory = false;
-  //       }
-  //     }
-  //     if (victory) return color;
-  //   }
-
-  //   //たて
-  //   for (let i = 0; i < 3; i++) {
-  //     let victory = true;
-  //     for (let j = 0; j < 3; j++) {
-  //       if (squares[j][i] !== color) {
-  //         victory = false;
-  //       }
-  //     }
-  //     if (victory) return color;
-  //   }
-
-  //   //斜め
-  //   let victory = true;
-  //   for (let i = 0; i < 3; i++) {
-  //     if (squares[i][i] !== color) {
-  //       victory = false;
-  //     }
-  //   }
-  //   if (victory) return color;
-  //   victory = true;
-  //   for (let i = 0; i < 3; i++) {
-  //     if (squares[2 - i][i] !== color) {
-  //       victory = false;
-  //     }
-  //   }
-  //   if (victory) return color;
-
-  //   return "";
-  // };
-
   useEffect(()=> {
     const judgeResult = judgeVictory(squares, turn);
     if (judgeResult !== "") {
